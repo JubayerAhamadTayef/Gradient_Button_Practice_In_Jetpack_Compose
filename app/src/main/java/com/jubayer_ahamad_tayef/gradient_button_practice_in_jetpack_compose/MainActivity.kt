0cc23b09627bf7c483ee8ba72e3285f55f0e866c
JubayerAhamadTayef <jubayerahmedtayef.579462@gmail.com>
2025-01-26 17:25:39 +0600

    Create and design Gradient Button as I need!

--- app/src/main/java/com/jubayer_ahamad_tayef/gradient_button_practice_in_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/jubayer_ahamad_tayef/gradient_button_practice_in_jetpack_compose/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jubayer_ahamad_tayef.gradient_button_practice_in_jetpack_compose.ui.theme.Color1
+import com.jubayer_ahamad_tayef.gradient_button_practice_in_jetpack_compose.ui.theme.Color2
 import com.jubayer_ahamad_tayef.gradient_button_practice_in_jetpack_compose.ui.theme.Gradient_Button_Practice_In_Jetpack_ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +32,42 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Gradient_Button_Practice_In_Jetpack_ComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyGradientButton(onClick = {})
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyGradientButton(
+    text: String = "Button",
+    textColor: Color = Color.White,
+    gradientColor: Brush = Brush.horizontalGradient(colors = listOf(Color1, Color2)),
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Button(colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+            contentPadding = PaddingValues(),
+            shape = RoundedCornerShape(10.dp),
+            onClick = { onClick() }) {
+            Box(
+                modifier = Modifier
+                    .background(gradientColor)
+                    .padding(horizontal = 16.dp, vertical = 8.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = text, color = textColor)
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    Gradient_Button_Practice_In_Jetpack_ComposeTheme {
-        Greeting("Android")
-    }
+fun MyGradientButtonPreview() {
+    MyGradientButton(onClick = {})
 }
